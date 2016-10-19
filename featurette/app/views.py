20c8f9d2a5e5d6477c9a146f828cb1d1371de271
suecarmol <scarmol@mit.edu>
2016-10-18 23:56:50 -0500

    Created preliminary form for feature requests

--- featurette/app/views.py
+++ featurette/app/views.py
@@ -4,7 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Open Susie'}  # fake user
-    return render_template('index.html',
-                           title='Featurette - Home',
-                           user=user)
+    return render_template('index.html')
