e47b0f283bfcf63c1bc4b2efc6e0b5b5a4f8a95e
Susana Cardenas Molinar <susana.cardenas@enable.com.mx>
2016-10-19 18:44:21 -0500

    Started creating models, connection to database

---
+++ featurette/app/models.py
@@ -0,0 +1,38 @@
+from app import db
+from sqlalchemy.sql import func
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+class FeatureRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(255))
+    #TODO: create foreign key
+    client_id = db.Column(db.Integer)
+    client_priority = db.Column(db.Integer)
+    #TODO: create foreign key
+    product_area = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+class ProductAre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
