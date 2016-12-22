397777cdd85091ed67a67b543eb74e8a9b600d79
Susana Cardenas Molinar <susana.cardenas@enable.com.mx>
2016-12-22 16:01:39 -0600

    Changed some functional tests

--- featurette/app/static/addClient.js
+++ featurette/app/static/addClient.js
@@ -10,19 +10,19 @@ $(document).ready(function(){
     //Adding client
 	$('#submit').click(function(){
         var client_name = $('#client_name').val();
-        console.log(client_name);
-        $.ajax({
-            url: '/api/v1/clients',
-            type: "POST",
-            data: {client_name : client_name},
-            success: function(data){
-                console.log('Client inserted successfully');
-            },
-            error: function(xhr,err){
-                console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
-                console.log("responseText: "+xhr.responseText);
-            }
-        });
+		if(client_name != null && client_name != ''){
+	        $.ajax({
+	            url: '/api/v1/clients',
+	            type: "POST",
+	            data: {client_name : client_name},
+	            success: function(data){
+	                console.log('Client inserted successfully');
+	            },
+	            error: function(xhr,err){
+	                console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+	                console.log("responseText: "+xhr.responseText);
+	            }
+	        });
+		}
 	});
-
 });
--- featurette/app/static/addProductArea.js
+++ featurette/app/static/addProductArea.js
@@ -10,19 +10,20 @@ $(document).ready(function(){
     //Adding product area
 	$('#submit').click(function(){
         var product_area = $('#product_area_name').val();
-        console.log(product_area);
-        $.ajax({
-            url: '/api/v1/productAreas',
-            type: "POST",
-            data: {product_area_name : product_area},
-            success: function(data){
-                console.log('Product Area inserted successfully');
-            },
-            error: function(xhr,err){
-                console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
-                console.log("responseText: "+xhr.responseText);
-            }
-        });
+        if(product_area != null && product_area != ''){
+			$.ajax({
+	            url: '/api/v1/productAreas',
+	            type: "POST",
+	            data: {product_area_name : product_area},
+	            success: function(data){
+	                console.log('Product Area inserted successfully');
+	            },
+	            error: function(xhr,err){
+	                console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+	                console.log("responseText: "+xhr.responseText);
+	            }
+	        });
+		}
 	});
 
 });
