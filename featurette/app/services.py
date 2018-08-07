ba6fe885c54af1ab7e7b06c7236ef6f0c0cf0e8c
suecarmol <scarmol@mit.edu>
2018-08-07 11:32:06 -0500

    Added many try excepts for SQL Alchemy sessions

--- featurette/app/services.py
+++ featurette/app/services.py
@@ -88,7 +88,12 @@ parser_feature.add_argument('ticket_url', type=str, required=True)
 
 @login_manager.user_loader
 def user_loader(user_id):
-    return session.query(User).get(user_id)
+    try:
+        return session.query(User).get(user_id)
+    except:
+        session.rollback()
+        print("Could not get user")
+        return {}, 500
 
 
 @login_manager.token_loader
@@ -99,7 +104,12 @@ def load_token(token):
     data = login_serializer.loads(token, max_age=max_age)
 
     # Find the User
-    user = session.query(User).get(data[0])
+    try:
+        user = session.query(User).get(data[0])
+    except:
+        session.rollback()
+        print("Could not get user")
+        return {}, 500
 
     # Check Password and return user or None
     if user and data[1] == user.password:
@@ -113,7 +123,12 @@ class LoginResource(Resource):
         parsed_args = parser_login.parse_args()
         email = parsed_args['email']
         password = parsed_args['password']
-        user = session.query(User).filter_by(email=email).first()
+        try:
+            user = session.query(User).filter_by(email=email).first()
+        except:
+            session.rollback()
+            print("User Login query not found")
+            return {}, 500
         if not user:
             abort(404, message="User {} doesn't exist".format(email))
         if bcrypt.check_password_hash(user.password, password):
@@ -145,14 +160,24 @@ class ClientResource(Resource):
     @login_required
     @marshal_with(client_fields)
     def get(self, id):
-        client = session.query(Client).get(id)
+        try:
+            client = session.query(Client).get(id)
+        except:
+            session.rollback()
+            print("Client in GET not found")
+            return {}, 500
         if not client:
             abort(404, message="Client {} doesn't exist".format(id))
         return client
 
     @login_required
     def delete(self, id):
-        client = session.query(Client).get(id)
+        try:
+            client = session.query(Client).get(id)
+        except:
+            session.rollback()
+            print("Client in DELETE not found")
+            return {}, 500
         if not client:
             abort(404, message="Client {} doesn't exist".format(id))
         try:
@@ -166,7 +191,14 @@ class ClientResource(Resource):
     @marshal_with(client_fields)
     def put(self, id):
         parsed_args = parser_client.parse_args()
-        client = session.query(Client).get(id)
+
+        try:
+            client = session.query(Client).get(id)
+        except:
+            session.rollback()
+            print("Error in PUT client")
+            return {}, 500
+
         client_name = parsed_args['client_name']
         if not client_name:
             return 418
@@ -184,7 +216,12 @@ class ClientListResource(Resource):
     @login_required
     @marshal_with(client_fields)
     def get(self):
-        clients = session.query(Client).all()
+        try:
+            clients = session.query(Client).all()
+        except:
+            session.rollback()
+            print("Error in GET all clients")
+            return {}, 500
         return clients
 
     @login_required
@@ -208,14 +245,24 @@ class ProductAreaResource(Resource):
     @login_required
     @marshal_with(product_area_fields)
     def get(self, id):
-        product_area = session.query(ProductArea).get(id)
+        try:
+            product_area = session.query(ProductArea).get(id)
+        except:
+            session.rollback()
+            print("Error in GET product Area")
+            return {}, 500
         if not product_area:
             abort(404, message="Product Area {} doesn't exist".format(id))
         return product_area
 
     @login_required
     def delete(self, id):
-        product_area = session.query(ProductArea).get(id)
+        try:
+            product_area = session.query(ProductArea).get(id)
+        except:
+            session.rollback()
+            print("Error in DELETE product area, product area not found")
+            return {}, 500
         if not product_area:
             abort(404, message="Product Area {} doesn't exist".format(id))
         try:
@@ -229,7 +276,12 @@ class ProductAreaResource(Resource):
     @marshal_with(product_area_fields)
     def put(self, id):
         parsed_args = parser_product_area.parse_args()
-        product_area = session.query(ProductArea).get(id)
+        try:
+            product_area = session.query(ProductArea).get(id)
+        except:
+            session.rollback()
+            print("Product area PUT failed, no product area found")
+            return {}, 500
         product_area_name = parsed_args['product_area_name']
         if not product_area_name:
             return 418
@@ -247,7 +299,12 @@ class ProductAreaListResource(Resource):
     @login_required
     @marshal_with(product_area_fields)
     def get(self):
-        product_areas = session.query(ProductArea).all()
+        try:
+            product_areas = session.query(ProductArea).all()
+        except:
+            session.rollback()
+            print("Product Areas not found")
+            return {}, 500
         return product_areas
 
     @login_required
@@ -271,14 +328,24 @@ class UserResource(Resource):
     @login_required
     @marshal_with(user_fields)
     def get(self, id):
-        user = session.query(User).get(id)
+        try:
+            user = session.query(User).get(id)
+        except:
+            session.rollback()
+            print("Error in GET user")
+            return {}, 500
         if not user:
             abort(404, message="User {} doesn't exist".format(id))
         return user
 
     @login_required
     def delete(self, id):
-        user = session.query(User).get(id)
+        try:
+            user = session.query(User).get(id)
+        except:
+            session.rollback()
+            print("Error in DELETE user")
+            return {}, 500
         if not user:
             abort(404, message="User {} doesn't exist".format(id))
         # current_user is not enabled during unit tests
@@ -296,7 +363,12 @@ class UserResource(Resource):
     @marshal_with(user_fields)
     def put(self, id):
         parsed_args = parser_user.parse_args()
-        user = session.query(User).get(id)
+        try:
+            user = session.query(User).get(id)
+        except:
+            session.rollback()
+            print("Error in PUT user")
+            return {}, 500
         username = parsed_args['username']
         email = parsed_args['email']
         password = bcrypt.generate_password_hash(parsed_args['password'])
@@ -318,7 +390,12 @@ class UserListResource(Resource):
     @login_required
     @marshal_with(user_fields)
     def get(self):
-        users = session.query(User).all()
+        try:
+            users = session.query(User).all()
+        except:
+            session.rollback()
+            print("Could not GET all users")
+            return {}, 500
         return users
 
     @login_required
@@ -344,7 +421,12 @@ class FeatureRequestResource(Resource):
     @login_required
     @marshal_with(feature_request_fields_user_friendly)
     def get(self, id):
-        feature_request = session.query(FeatureRequest).get(id)
+        try:
+            feature_request = session.query(FeatureRequest).get(id)
+        except:
+            session.rollback()
+            print("Error in GET feature request")
+            return {}, 500
         if not feature_request:
             abort(404, message="Feature request {} doesn't exist".format(id))
         feature_request_user_friendly = {}
@@ -367,7 +449,12 @@ class FeatureRequestResource(Resource):
 
     @login_required
     def delete(self, id):
-        feature_request = session.query(FeatureRequest).get(id)
+        try:
+            feature_request = session.query(FeatureRequest).get(id)
+        except:
+            session.rollback()
+            print("Error in DELETE feature request")
+            return {}, 500
         if not feature_request:
             abort(404, message="Feature request {} doesn't exist".format(id))
         try:
@@ -386,7 +473,12 @@ class FeatureRequestResource(Resource):
         client_priority = parsed_args['client_priority']
         conv_target_date = datetime.strptime(parsed_args['target_date'],
                                              "%Y-%m-%d %H:%M")
-        feature_request = session.query(FeatureRequest).get(id)
+        try:
+            feature_request = session.query(FeatureRequest).get(id)
+        except:
+            session.rollback()
+            print("Error in PUT feature request")
+            return {}, 500
         feature_request.title = title
         feature_request.description = parsed_args['description']
         feature_request.client_id = client_id
@@ -413,7 +505,11 @@ class FeatureRequestResource(Resource):
         # initializing priorities dictionary
         priorities_dict = {}
         # find all active feature requests (with date_finished = None)
-        features_same_client = session.query(FeatureRequest).filter(FeatureRequest.client_id == client_id).filter(FeatureRequest.date_finished == None) # noqa
+        try:
+            features_same_client = session.query(FeatureRequest).filter(FeatureRequest.client_id == client_id).filter(FeatureRequest.date_finished == None) # noqa
+        except:
+            session.rollback()
+            print("Error in check priorities feature request")
         # filling dictionary
         for feature_same_client in features_same_client:
             priorities_dict[str(feature_same_client.client_priority)] = feature_same_client.title # noqa
@@ -433,14 +529,23 @@ class FeatureRequestResource(Resource):
             # add old priority and title
             priorities_dict[str(old_key)] = old_title
             # get old Feature Request that matches the parameters
-            feature_request = session.query(FeatureRequest).filter(FeatureRequest.title == old_title).filter(FeatureRequest.client_priority == new_client_priority).filter(FeatureRequest.client_id == client_id).first() # noqa
+            try:
+                feature_request = session.query(FeatureRequest).filter(FeatureRequest.title == old_title).filter(FeatureRequest.client_priority == new_client_priority).filter(FeatureRequest.client_id == client_id).first() # noqa
+            except:
+                session.rollback()
+                print("Error in second query of check priorities")
 
 
 class FinishFeatureResource(Resource):
     @login_required
     @marshal_with(feature_request_fields)
     def post(self, id):
-        feature_request = session.query(FeatureRequest).get(id)
+        try:
+            feature_request = session.query(FeatureRequest).get(id)
+        except:
+            session.rollback()
+            print("Error in finish feature request")
+            return {}, 500
         if not feature_request:
             abort(404, message="Feature request {} doesn't exist".format(id))
         feature_request.date_finished = str(datetime.now())
@@ -456,7 +561,12 @@ class FeatureRequestListResource(Resource):
     @login_required
     @marshal_with(feature_request_fields_user_friendly)
     def get(self):
-        feature_requests = session.query(FeatureRequest).all()
+        try:
+            feature_requests = session.query(FeatureRequest).all()
+        except:
+            session.rollback()
+            print("Error in GET all feature requests")
+            return {}, 500
         format_feature_requests = []
         for feature_request in feature_requests:
             tmp_feature_request = {}
@@ -521,7 +631,11 @@ class FeatureRequestListResource(Resource):
         # initializing priorities dictionary
         priorities_dict = {}
         # find all active feature requests (with date_finished = None)
-        features_same_client = session.query(FeatureRequest).filter(FeatureRequest.client_id == client_id).filter(FeatureRequest.date_finished == None) # noqa
+        try:
+            features_same_client = session.query(FeatureRequest).filter(FeatureRequest.client_id == client_id).filter(FeatureRequest.date_finished == None) # noqa
+        except:
+            session.rollback()
+            print("Error in check priorities pt 2 - 1")
         # filling dictionary
         for feature_same_client in features_same_client:
             priorities_dict[str(feature_same_client.client_priority)] = feature_same_client.title # noqa
@@ -541,7 +655,11 @@ class FeatureRequestListResource(Resource):
             # add old priority and title
             priorities_dict[str(old_key)] = old_title
             # get old Feature Request that matches the parameters
-            feature_request = session.query(FeatureRequest).filter(FeatureRequest.title == old_title).filter(FeatureRequest.client_priority == new_client_priority).filter(FeatureRequest.client_id == client_id).first() # noqa
+            try:
+                feature_request = session.query(FeatureRequest).filter(FeatureRequest.title == old_title).filter(FeatureRequest.client_priority == new_client_priority).filter(FeatureRequest.client_id == client_id).first() # noqa
+            except:
+                session.rollback()
+                print("Error in check prioritues pt 2 - 2")
 
             feature_request.client_priority = int(old_key)
             try:
